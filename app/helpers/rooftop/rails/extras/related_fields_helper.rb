2f2b31ff3f4fdbcf73152c817fb4d9361b9c9f34
Ed Jones <ed@errorstudio.co.uk>
2016-03-04 16:16:37 +0000

    yank out the value from the hash in the field_from_related_resource helper

--- app/helpers/rooftop/rails/extras/related_fields_helper.rb
+++ app/helpers/rooftop/rails/extras/related_fields_helper.rb
@@ -5,7 +5,7 @@ module Rooftop
         def field_from_related_resource(resource, field)
           field_data = resource[:advanced].select {|f| f[:fields].collect {|f| f[:name] == field.to_s}.any?}
           if field_data.any?
-            field_data.first[:fields].find {|f| f[:name] == field.to_s}
+            field_data.first[:fields].find {|f| f[:name] == field.to_s}.try(:[],:value)
           end
         end
       end
