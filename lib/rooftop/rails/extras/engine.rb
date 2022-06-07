9bd80a39f4fad3f977ff8fe16e42f3fde63db07b
Sebastian Iorga <sebastian@neudata.co>
2022-06-07 15:43:01 +0100

    Debug slowness

--- lib/rooftop/rails/extras/engine.rb
+++ lib/rooftop/rails/extras/engine.rb
@@ -6,6 +6,12 @@ module Rooftop
         isolate_namespace Rooftop::Rails::Extras
 
         config.before_initialize do
+          # ::Rails.application.eager_load!
+          Rooftop::Page.extra_includes ||= []
+          Rooftop::Page.extra_includes << Rooftop::Rails::Extras::PageRedirect
+
+          Rooftop::Nested.extra_includes ||= []
+          Rooftop::Nested.extra_includes << Rooftop::Rails::Extras::ResolvedChildren
 
         end
 
@@ -18,17 +24,7 @@ module Rooftop
         end
 
        config.to_prepare do
-          ::Rails.application.eager_load!
-          if Rooftop::Page.page_classes.present?
-            Rooftop::Page.page_classes.each do |klass|
-              klass.send(:include, Rooftop::Rails::Extras::PageRedirect)
-            end
-          end
-          if Rooftop::Nested.nested_classes.present?
-            Rooftop::Nested.nested_classes.each do |klass|
-              klass.send(:include, Rooftop::Rails::Extras::ResolvedChildren)
-            end
-          end
+
         end
 
       end
